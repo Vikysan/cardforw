11794a18a08b0b95eb86ab52ef269a4a31e43d46
Vikysan <vikysamachan@gmail.com>
2023-03-10 23:28:19 +0100

    uprava select input

--- src/select.js
+++ src/select.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import words from './data/words.json'
 
 const Select = (props) => {
@@ -6,6 +6,7 @@ const Select = (props) => {
     const [files] = useState([...new Set(words.map(item=>item.file))])
     const [topics,setTopics] = useState([...new Set((words.filter(item=>item.file==='1')).map(item=>item.topic))])
     const [currentFile,setCurrentFile] = useState('1')
+    const [currentTopic,setCurrentTopic] = useState("Food and Cooking")
 
     const onChange = (event) => {
         const value = event.target.value;
@@ -15,17 +16,26 @@ const Select = (props) => {
 
     const onChangeTopics = (event) =>{
         const value = event.target.value;
-      
+        setCurrentTopic(value)
         props.changeTopic(value,currentFile)
     }
+
+    useEffect(() => {
+      const localTopic = JSON.parse(localStorage.getItem('topic'));
+      if (localTopic) {
+        setTopics([...new Set((words.filter(item=>item.file===localTopic.file)).map(item=>item.topic))])
+        setCurrentFile(localTopic.file)
+        setCurrentTopic(localTopic.topic)
+      }
+    }, []);
 
   return (
     <div>
       <select name="File" id="File" onChange={onChange}>
-        {[...files].map((item,index)=><option value={item} key={'f' + index}>{item}</option>)}
+        {files.map((item,index)=><option value={item} key={'f' + index} selected={currentFile===item} >{item}</option>)}
         </select>
         <select name="Topic" id="Topic" onChange={onChangeTopics}>
-        {[...topics].map((item,index)=><option value={item} key={'t'+ index}>{item}</option>)}
+        {topics.map((item,index)=><option value={item} key={'t'+ index} selected={currentTopic===item}>{item}</option>)}
         </select>
     </div>
   )
